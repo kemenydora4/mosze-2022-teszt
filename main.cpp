115a23a6fdd7e87a423feb56315441f1ce861496
kemenydora4 <113248455+kemenydora4@users.noreply.github.com>
2022-09-19 21:18:02 +0200

    Hibak kommentezese

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // Az alulvonas hianyzik
+    std::cout << '1-100 ertekek duplazasa' //Az aposztrof jel rossz, helyette " ", nincs ; a sor vegen
+    for (int i = 0;) // A ciklusnak csak kezdoerteke van
     {
-        b[i] = i * 2;
+        b[i] = i * 2; // 0 es 99 kozott duplaz, ehelyett 1 es 100 kozott kellene
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)// A feltetel mindig hamis lesz, be sem lep a ciklusba
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //Az erteket nem irjuk ki, nincs end es pontosvesszo
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; // Nincs kezdoerteke az atlagnak
+    for (int i = 0; i < N_ELEMENTS, i++) //az N_ELEMENTS utan pontosvesszot kell irni
     {
-        atlag += b[i]
+        atlag += b[i] //hianyzik a pontosvesszo
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
